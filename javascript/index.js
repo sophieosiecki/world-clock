6e908af37828fd5fabc70a7f3f8662cf26a7c506
Sophie Osiecki <sophie.osiecki@icloud.com>
2024-11-10 13:29:13 +0000

    Functionality to update the seconds of the city chosen in the dropdown menu and other small edits.

--- javascript/index.js
+++ javascript/index.js
@@ -2,13 +2,14 @@
 let montrealElement = document.querySelector("#montreal");
 let londonElement = document.querySelector("#london");
 let tokyoElement = document.querySelector("#tokyo");
+let citiesElement = document.querySelector("#cities");
 
 // London Time
 function updateLondonTime() {
   if (londonElement.length != 0) {
     let londonDateElement = londonElement.querySelector(".date");
     let londonTimeElement = londonElement.querySelector(".time");
-    let londonTime = moment().tz("Europe/London");
+    let londonTime = moment.tz("Europe/London");
 
     londonDateElement.innerHTML = londonTime.format("dddd Do MMMM YYYY");
     londonTimeElement.innerHTML = `${londonTime.format("HH:mm:ss")}`;
@@ -19,7 +20,7 @@ function updateLondonTime() {
 function updateMontrealTime() {
   let montrealDateElement = montrealElement.querySelector(".date");
   let montrealTimeElement = montrealElement.querySelector(".time");
-  let montrealTime = moment().tz("America/Montreal");
+  let montrealTime = moment.tz("America/Montreal");
 
   montrealDateElement.innerHTML = montrealTime.format("dddd Do MMMM YYYY");
   montrealTimeElement.innerHTML = `${montrealTime.format("HH:mm:ss")}`;
@@ -29,46 +30,61 @@ function updateMontrealTime() {
 function updateTokyoTime() {
   let tokyoDateElement = tokyoElement.querySelector(".date");
   let tokyoTimeElement = tokyoElement.querySelector(".time");
-  let tokyoTime = moment().tz("Asia/Tokyo");
+  let tokyoTime = moment.tz("Asia/Tokyo");
 
   tokyoDateElement.innerHTML = tokyoTime.format("dddd Do MMMM YYYY");
   tokyoTimeElement.innerHTML = `${tokyoTime.format("HH:mm:ss")}`;
 }
 
 // Selected City time
+
+let cityIntervalId; // Global variable to store the interval ID
+
 function updateNewCity(event) {
+  // Clear the previous interval if it exists
+  if (cityIntervalId) {
+    clearInterval(cityIntervalId); // Stop the previous interval
+  }
+
+  // Capture the selected city's timezone and name
   let cityTimeZone = event.target.value;
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  citiesElement = document.querySelector("#cities");
-  console.log(event);
-  citiesElement.innerHTML = `
-  <div class="city">
-                <div>
-                    <h2>${cityName}</h2>
-                    <div class="date">${cityTime.format(
-                      "dddd Do MMMM YYYY"
-                    )}</div>
-                </div>
-                <div class="time">${cityTime.format("HH:mm:ss")}</div>
-            </div>
-  `;
+  let citiesElement = document.querySelector("#cities");
+
+  // Define a function to update the city's date and time every second
+  function updateCityInterval() {
+    let cityTime = moment.tz(cityTimeZone); // Get the current time for the selected city
+
+    // Update the inner HTML of the target element
+    citiesElement.innerHTML = `
+      <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("dddd Do MMMM YYYY")}</div>
+        </div>
+        <div class="time" id="current-city-time">${cityTime.format(
+          "HH:mm:ss"
+        )}</div>
+      </div>
+    `;
+  }
+  cityIntervalId = setInterval(updateCityInterval, 1000);
 }
 
 let selectElement = document.querySelector("#new-city");
 selectElement.addEventListener("change", updateNewCity);
 
 updateLondonTime();
-if (londonElement.length != 0) {
+if (londonElement) {
   setInterval(updateLondonTime, 1000);
 }
 
 updateMontrealTime();
-if (montrealElement.length != 0) {
+if (montrealElement) {
   setInterval(updateMontrealTime, 1000);
 }
 
 updateTokyoTime();
-if (tokyoElement.length != 0) {
+if (tokyoElement) {
   setInterval(updateTokyoTime, 1000);
 }
